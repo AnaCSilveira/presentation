f442b8441235643e35a99c51235b019498279e79
AnaCSilveira <charlesassuncao@Charless-MacBook-Pro.local>
2022-05-02 01:32:16 -0300

    chore: seed

---
+++ index.js
@@ -0,0 +1,6 @@
+function displayAbout() {
+    const arrow = document.querySelector("#animation");
+    arrow.classList.toggle("rotate");
+    const about =document.querySelector(".about");
+    about.classList.toggle("show")
+}
